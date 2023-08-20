c846168e799b00dcac1b2a5efd1b48aa2b25bacd
nursultanjumashov <nursultanjumashovv@gmail.com>
2023-08-20 16:04:33 +0600

    int

--- src/main/kotlin/variables.kt
+++ src/main/kotlin/variables.kt
@@ -1,5 +1,35 @@
 fun funVariables() {
-    val age: Int
-    age = 23
-    println(age)
+//    val age: Int
+//    age = 23
+//    println(age)
+
+//
+//    val a: UByte = 1U
+//    val b: Short = 45
+//    val c: Int = 250333332
+//    val d: Long = 30000
+//    println(a) // -10
+//    println(b) // 45
+//    println(c) // -250
+//    println(d) // 30000
+
+//
+//    val a: UByte = 10U
+//    val b: UShort = 45U
+//    val c: UInt = 250U
+//    val d: ULong = 30000U
+//    println(a) // 10
+//    println(b) // 45
+//    println(c) // 250
+//    println(d) // 30000
+
+//
+//    val address: Int = 0x0A1
+//    println(address)
+
+
+    val a: Int = 0b0101    // 5
+    val b: Int = 0b1011     // 11
+    println(a)      // 5
+    println(b)      // 11
 }
